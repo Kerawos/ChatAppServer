899cc4e3df7b8b74d1a6ea955b11f07b349d36d4
Kerawos <Maresowa@gmail.com>
2017-09-10 12:53:23 +0200

    Initial commit

---
+++ src/main/java/pl/mareksowa/SerwerChat/StarterServerChatApplication.java
@@ -0,0 +1,12 @@
+package pl.mareksowa.SerwerChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterServerChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterServerChatApplication.class, args);
+	}
+}
