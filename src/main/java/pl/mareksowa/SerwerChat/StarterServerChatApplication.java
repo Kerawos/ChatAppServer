7c693fb7b11d07e6def8d5ee6cc0d8177b410241
Kerawos <Maresowa@gmail.com>
2018-05-18 16:30:22 +0200

    update jar

--- src/main/java/pl/mareksowa/SerwerChat/StarterServerChatApplication.java
+++ src/main/java/pl/mareksowa/SerwerChat/StarterServerChatApplication.java
@@ -1,9 +1,11 @@
 package pl.mareksowa.SerwerChat;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class StarterServerChatApplication {
 
 	public static void main(String[] args) {
